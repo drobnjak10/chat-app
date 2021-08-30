ebf4e69d4194bebc83fc897a673f5b3cce45ba48
drobnjakdavid <drobnjakdavid@gmail.com>
2021-08-30 09:19:03 +0200

    =Init commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicDir = path.join(__dirname, '../public');
+
+// app.use(express.json());
+app.use(express.static(publicDir))
+
+app.get('', async (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log('Server is up on the port', port);
+})
